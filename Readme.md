aa8455d0b78c31f3854aea79d186d20aa213570f
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2022-06-10 00:32:58 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# ConsoleUI
+
+This project contains some tests and implementation templates to get a rich and upgraded user experience in terminal applications. 
+Any application that just needs some user interaction can be improved with some little tweaks.
+
+Applications that inspired this project are for example:
+* docker cli: When running docker build, the output stays mostly inside the current console screen without scrolling. Instead the log messages run through in the range of 10-15 lines in between. 
+
+Possible libraries/frameworks
+* [gui.cs](https://github.com/migueldeicaza/gui.cs): Complete TUI imoplementation using ncurses on Unix 
+* [Spectre.Console](https://spectreconsole.net/quick-start): Modern console features for improved user experience
+* [Sharprompt](https://github.com/shibayan/Sharprompt): Multiple prompt options for user input
+
+Resources
+* [Awesome console apps in C#](https://piraces.dev/posts/awesome-console-apps-csharp/) 
+* [How YOU can build a great looking .NET Console app with Spectre](https://dev.to/dotnet/how-you-can-build-a-great-looking-net-console-app-with-spectre-2f6g)
+* [How to build an interactive command-line application in .NET](https://dev.to/shibayan/how-to-build-an-interactive-command-line-application-in-net-4oic)
---
+++ SpectreTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Write(
+    new FigletText("Scaffold-demo")
+        .LeftAligned()
+        .Color(Color.Red));
+
+var answerReadme = AnsiConsole.Confirm("Generate a [green]README[/] file?");
+
+var answerGitIgnore = AnsiConsole.Confirm("Generate a [yellow].gitignore[/] file?");
+
+var framework = AnsiConsole.Prompt(
+  new SelectionPrompt<string>()
+      .Title("Select [green]test framework[/] to use")
+      .PageSize(10)
+      .MoreChoicesText("[grey](Move up and down to reveal more frameworks)[/]")
+      .AddChoices(new[] {
+          "XUnit", "NUnit","MSTest"
+      }));
+
+AnsiConsole.MarkupLine($"[green]{framework} selected.[/]");
+
+AnsiConsole.Status()
+.Start("Generating project...", ctx =>
+{
+  if(answerReadme) 
+  {
+    ctx.Spinner(Spinner.Known.Aesthetic);
+    ctx.SpinnerStyle(Style.Parse("green"));
+
+    AnsiConsole.MarkupLine("LOG: Creating README ...");
+    Thread.Sleep(1000);
+    // Update the status and spinner
+    ctx.Status("Next task");
+    Thread.Sleep(1000);
+    // Update the status and spinner
+    ctx.Status("Another task" + Environment.NewLine + "With multiple lines");
+    Thread.Sleep(1000);
+    // Update the status and spinner
+    ctx.Status("And the last task");
+  }
+
+  if(answerGitIgnore) 
+  {
+    ctx.Spinner(Spinner.Known.Dots8Bit);
+    ctx.SpinnerStyle(Style.Parse("orange1"));
+
+    AnsiConsole.MarkupLine("LOG: Creating .gitignore ...");
+    Thread.Sleep(1000);
+    // Update the status and spinner
+    ctx.Status("Next task");
+  }
+
+  // Simulate some work
+  AnsiConsole.MarkupLine("LOG: Configuring test framework...");
+  Thread.Sleep(2000);
+});
+
+// Synchronous
+AnsiConsole.Progress()
+    .Start(ctx => 
+    {
+        // Define tasks
+        var task1 = ctx.AddTask("[green]Reticulating splines[/]");
+        var task2 = ctx.AddTask("[green]Folding space[/]");
+
+        while(!ctx.IsFinished) 
+        {
+            task1.Increment(1.5);
+            task2.Increment(0.5);
+            Thread.Sleep(100);
+        }
+    });
